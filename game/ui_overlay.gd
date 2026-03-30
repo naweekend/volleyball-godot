2fcf0148e13942f4997f4fcfd2bda25cb043203e
Naweekend <nabeel.weekend@gmail.com>
2026-03-30 20:25:08 +0500

    added pause

--- game/ui_overlay.gd
+++ game/ui_overlay.gd
@@ -3,9 +3,11 @@ extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	process_mode = Node.PROCESS_MODE_ALWAYS
 	GameManager.update_labels()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	if Input.is_action_just_pressed("pause"):
+		get_tree().paused = not get_tree().paused
